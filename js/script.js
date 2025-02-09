ed5b1521594482f35b0b9b37f8fc831054dde691
pointofvu <prince1jpg@gmail.com>
2025-02-10 01:52:47 +0530

    some final touches

--- js/script.js
+++ js/script.js
@@ -8,3 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   
 
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8KNWJYHCD5');
